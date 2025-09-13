ec14a735130c65108547b756e1a22b4154a9c599
Alan Delage <alan.delage@ynov.com>
2025-09-13 18:35:51 +0100

    character

---
+++ character/main.go
@@ -0,0 +1,78 @@
+package main
+
+type Character1 struct {
+	Nom        string
+	Classe     string
+	Niveau     int
+	Pv         int
+	PvMax      int
+	Inventaire []Item
+}
+
+type Character2 struct {
+	Nom        string
+	Classe     string
+	Niveau     int
+	Pv         int
+	PvMax      int
+	Mana       int
+	Inventaire []Item
+}
+
+type Character3 struct {
+	Nom        string
+	Classe     string
+	Niveau     int
+	Pv         int
+	PvMax      int
+	Inventaire []Item
+}
+
+type Item struct {
+	Nom      string
+	Quantite int
+}
+
+func (player *Character1) initCharacter() {
+	*player = Character1{
+		Nom:    "Chris",
+		Classe: "Humain",
+		Niveau: 1,
+		Pv:     30,
+		PvMax:  200,
+		Inventaire: []Item{
+			{"Potion de vie", 2},
+			{"Pièces d'or", 30},
+		},
+	}
+}
+
+func (player *Character2) initCharacter() {
+	*player = Character2{
+		Nom:    "Celeste",
+		Classe: "Mage",
+		Niveau: 1,
+		Pv:     35,
+		PvMax:  250,
+		Mana:   30,
+		Inventaire: []Item{
+			{"Potion de vie", 2},
+			{"Potion de mana", 1},
+			{"Pièces d'or", 20},
+		},
+	}
+}
+
+func (player *Character3) initCharacter() {
+	*player = Character3{
+		Nom:    "Genzo",
+		Classe: "Samourai",
+		Niveau: 1,
+		Pv:     40,
+		PvMax:  300,
+		Inventaire: []Item{
+			{"Potion de vie", 1},
+			{"Pièces d'or", 20},
+		},
+	}
+}
