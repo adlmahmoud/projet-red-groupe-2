4a0899db53b4f0ba70af2a3b894fd99b8043bf65
Alan Delage <alan.delage@ynov.com>
2025-09-17 14:45:18 +0100

    monstre

--- src/monstre/monstre.go
+++ src/monstre/monstre.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type MOB struct {
 	Nom   string
 	Pv    int
@@ -7,126 +9,58 @@ type MOB struct {
 	Degat int
 }
 
+func (mob *MOB) InitGobelin() {
+	*mob = MOB{"Gobelin d'entraînement", 40, 40, 5}
+}
 func (mob *MOB) InitBrigand() {
-	*mob = MOB{
-		Nom:   "Brigand",
-		Pv:    60,
-		PvMax: 60,
-		Degat: 10,
-	}
+	*mob = MOB{"Brigand", 60, 60, 10}
 }
 func (mob *MOB) InitVoleur() {
-	*mob = MOB{
-		Nom:   "Voleur",
-		Pv:    60,
-		PvMax: 60,
-		Degat: 20,
-	}
+	*mob = MOB{"Voleur", 60, 60, 20}
 }
 func (mob *MOB) InitRhinoppotame() {
-	*mob = MOB{
-		Nom:   "Rhinoppotame",
-		Pv:    105,
-		PvMax: 105,
-		Degat: 25,
-	}
+	*mob = MOB{"Rhinoppotame", 105, 105, 25}
 }
 func (mob *MOB) InitChienzombie() {
-	*mob = MOB{
-		Nom:   "Chien zombie",
-		Pv:    105,
-		PvMax: 105,
-		Degat: 30,
-	}
+	*mob = MOB{"Chien zombie", 105, 105, 30}
 }
 func (mob *MOB) InitSquelettemage() {
-	*mob = MOB{
-		Nom:   "Squelette Mage",
-		Pv:    210,
-		PvMax: 210,
-		Degat: 30,
-	}
+	*mob = MOB{"Squelette Mage", 210, 210, 30}
 }
 func (mob *MOB) InitMortvivant() {
-	*mob = MOB{
-		Nom:   "Mort-Vivant",
-		Pv:    210,
-		PvMax: 210,
-		Degat: 40,
-	}
+	*mob = MOB{"Mort-Vivant", 210, 210, 40}
 }
-func (mob *MOB) InitScorpionaraignée() {
-	*mob = MOB{
-		Nom:   "Scorpion-Araignée",
-		Pv:    280,
-		PvMax: 280,
-		Degat: 40,
-	}
+func (mob *MOB) InitScorpionaraignee() {
+	*mob = MOB{"Scorpion-Araignée", 280, 280, 40}
 }
-func (mob *MOB) InitCrabegéant() {
-	*mob = MOB{
-		Nom:   "Crabe géant",
-		Pv:    310,
-		PvMax: 310,
-		Degat: 50,
-	}
+func (mob *MOB) InitCrabegeant() {
+	*mob = MOB{"Crabe géant", 310, 310, 50}
 }
 func (mob *MOB) InitBasilic() {
-	*mob = MOB{
-		Nom:   "Basilc",
-		Pv:    350,
-		PvMax: 350,
-		Degat: 50,
-	}
+	*mob = MOB{"Basilic", 350, 350, 50}
 }
 func (mob *MOB) InitTroll() {
-	*mob = MOB{
-		Nom:   "Troll",
-		Pv:    600,
-		PvMax: 600,
-		Degat: 60,
-	}
+	*mob = MOB{"Troll", 600, 600, 60}
 }
 func (mob *MOB) InitChauvesouris() {
-	*mob = MOB{
-		Nom:   "Chauvre-souris",
-		Pv:    630,
-		PvMax: 630,
-		Degat: 60,
-	}
+	*mob = MOB{"Chauve-souris", 630, 630, 60}
 }
 func (mob *MOB) InitFaucondecombat() {
-	*mob = MOB{
-		Nom:   "Faucon de combat",
-		Pv:    700,
-		PvMax: 700,
-		Degat: 70,
-	}
+	*mob = MOB{"Faucon de combat", 700, 700, 70}
 }
 func (mob *MOB) InitMarionnettesoldat() {
-	*mob = MOB{
-		Nom:   "Marionnette soldat",
-		Pv:    805,
-		PvMax: 805,
-		Degat: 80,
-	}
+	*mob = MOB{"Marionnette soldat", 805, 805, 80}
 }
-func (mob *MOB) InitCavaliersanstête() {
-	*mob = MOB{
-		Nom:   "Cavalier sans tête",
-		Pv:    835,
-		PvMax: 835,
-		Degat: 80,
-	}
+func (mob *MOB) InitCavaliersanstete() {
+	*mob = MOB{"Cavalier sans tête", 835, 835, 80}
 }
 func (mob *MOB) InitChevaliernoir() {
-	*mob = MOB{
-		Nom:   "Chevalier noir",
-		Pv:    950,
-		PvMax: 950,
-		Degat: 90,
-	}
+	*mob = MOB{"Chevalier noir", 950, 950, 90}
 }
+
+// =================================
+// BOSS
+// =================================
 
 type Boss struct {
 	Nom   string
@@ -135,51 +69,66 @@ type Boss struct {
 	Degat int
 }
 
-func (mob *Boss) initChavrot() {
-	*mob = Boss{
-		Nom:   "Chavrot",
-		Pv:    100,
-		PvMax: 100,
-		Degat: 25,
-	}
+func (b *Boss) InitChavrot() {
+	*b = Boss{"Chavrot", 100, 100, 25}
 }
-func (mob *Boss) initMoonlight() {
-	*mob = Boss{
-		Nom:   "Moonlight",
-		Pv:    200,
-		PvMax: 200,
-		Degat: 35,
-	}
+func (b *Boss) InitMoonlight() {
+	*b = Boss{"Moonlight", 200, 200, 35}
 }
-func (mob *Boss) initAinzolgone() {
-	*mob = Boss{
-		Nom:   "Ainzolgone",
-		Pv:    250,
-		PvMax: 250,
-		Degat: 45,
-	}
+func (b *Boss) InitAinzolgone() {
+	*b = Boss{"Ainzolgone", 250, 250, 45}
 }
-func (mob *Boss) initBahamut() {
-	*mob = Boss{
-		Nom:   "Bahamut",
-		Pv:    650,
-		PvMax: 650,
-		Degat: 105,
-	}
+func (b *Boss) InitBahamut() {
+	*b = Boss{"Bahamut", 650, 650, 105}
 }
-func (mob *Boss) initNegal() {
-	*mob = Boss{
-		Nom:   "Negal",
-		Pv:    800,
-		PvMax: 800,
-		Degat: 165,
-	}
+func (b *Boss) InitNegal() {
+	*b = Boss{"Negal", 800, 800, 165}
 }
-func (mob *Boss) initAchlys() {
-	*mob = Boss{
-		Nom:   "Achlys",
-		Pv:    1200,
-		PvMax: 1200,
-		Degat: 240,
+func (b *Boss) InitAchlys() {
+	*b = Boss{"Achlys", 1200, 1200, 240}
+}
+
+// =================================
+// MAIN
+// =================================
+
+func main() {
+	// Initialisation des 16 mobs
+	var mobs [16]MOB
+	mobs[0].InitGobelin()
+	mobs[1].InitBrigand()
+	mobs[2].InitVoleur()
+	mobs[3].InitRhinoppotame()
+	mobs[4].InitChienzombie()
+	mobs[5].InitSquelettemage()
+	mobs[6].InitMortvivant()
+	mobs[7].InitScorpionaraignee()
+	mobs[8].InitCrabegeant()
+	mobs[9].InitBasilic()
+	mobs[10].InitTroll()
+	mobs[11].InitChauvesouris()
+	mobs[12].InitFaucondecombat()
+	mobs[13].InitMarionnettesoldat()
+	mobs[14].InitCavaliersanstete()
+	mobs[15].InitChevaliernoir()
+
+	// Initialisation des 6 boss
+	var bosses [6]Boss
+	bosses[0].InitChavrot()
+	bosses[1].InitMoonlight()
+	bosses[2].InitAinzolgone()
+	bosses[3].InitBahamut()
+	bosses[4].InitNegal()
+	bosses[5].InitAchlys()
+
+	// Affichage de test
+	fmt.Println("===== MOBS =====")
+	for i, mob := range mobs {
+		fmt.Printf("[%2d] %-25s | PV: %4d/%-4d | Dégâts: %3d\n", i+1, mob.Nom, mob.Pv, mob.PvMax, mob.Degat)
+	}
+
+	fmt.Println("\n===== BOSSES =====")
+	for i, boss := range bosses {
+		fmt.Printf("[%2d] %-25s | PV: %4d/%-4d | Dégâts: %3d\n", i+1, boss.Nom, boss.Pv, boss.PvMax, boss.Degat)
 	}
 }
